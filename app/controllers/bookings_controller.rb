2e4de1d0dc87f82590827d4794dbe7232b6b1c39
Hyacinthe Aubert <hyacinthe.aubert@outlook.fr>
2025-03-04 16:15:37 +0100

    Commit to get the edit routes of booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,12 @@
  class BookingsController < ApplicationController
-#   before_action :authenticate_user, :only [:create, :update, :cancel]
-#   before_action :set_booking, :only [:show, :edit, :update, :cancel]
+  # before_action :set_booking, only: [:show, :update, :cancel]
 
-#   def index
-#     @bookings = Booking.all
-#   end
+  def index
+    @bookings = Booking.all
+  end
 
-#   def show
-#   end
+  def show
+  end
 
   def new
     @palace = Palace.find(params[:palace_id])
@@ -15,22 +14,29 @@
   end
 
   def create
+
+    @palace = Palace.find(params[:palace_id])
     @booking = Booking.new(booking_params)
     @booking.user = current_user
-    if @booking.save!
-      redirect_to booking_path(@booking)
+    @booking.palace = @palace
+
+    if @booking.save
+      redirect_to palace_path(@palace)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-#   def edit
-#   end
+  def edit
+  end
 
-#   def update
-#     @booking.update(booking_params)
-#     redirect_to booking_path(@booking)
-#   end
+  def update
+    if @booking.update(booking_params)
+      redirect_to booking_path(@booking)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
 #   def cancel
 #     @booking.status = false
@@ -40,10 +46,10 @@
 
   private
   def booking_params
-    params.require(:booking).permit(:price, :begin_date, :end_date, :palace_id)
+    params.require(:booking).permit(:begin_date, :end_date, :palace_id)
   end
 
-#   def set_booking
-#     @booking = Booking.find(params[:id])
-#   end
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :palace
 
-  validates :price, :begin_date, :end_date, presence: true
+  validates :begin_date, :end_date, presence: true
 end
