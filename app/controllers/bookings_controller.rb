0d3df904062494d48745b8b66562b0a666117e06
Hyacinthe Aubert <hyacinthe.aubert@outlook.fr>
2025-03-04 11:52:08 +0100

    CreateViewBooking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,9 +9,9 @@
 #   def show
 #   end
 
-#   def new
-#     @booking = Booking.new
-#   end
+  def new
+    @booking = Booking.new
+  end
 
 #   def create
 #     @booking = Booking.new(booking_params)
